8506fead4993ff315cb2bc2c44c679a26eb24b28
sreddyiitr <sunilleo@gmail.com>
2022-12-12 23:08:04 -0600

    Update RightAlignedPyramid.java

--- RightAlignedPyramid.java
+++ RightAlignedPyramid.java
@@ -10,19 +10,19 @@ public class RightAlignedPyramid
       
 			/* Inner For Loop (To print space)
 			   No of spaces in each row = (totalNumberOfRows - RowNumber) where RowNumber is i 
-         Example: 3 spaces in Row 1 (4-1), 2 spaces in row 2 (4-2) etc 
-      */
+			   Example: 3 spaces in Row 1 (4-1), 2 spaces in row 2 (4-2) etc 
+			*/
 			for(int j=(totalNumberOfRows-i); j>=1; j--)
 			{
 				System.out.print(" ");
 			}
-			
+
 			// Inner For Loop (To print star)
 			for(int j=1; j<=i; j++)
 			{
 				System.out.print("*");
 			}
-			
+
 			// ending line after each row
 			System.out.println();
 		}
